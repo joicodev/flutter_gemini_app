7e022fec0b3f50f15282b2237c7fb700f5931490
Roimer Peraza <roimerperaza@MacBookPro.attlocal.net>
2025-04-18 23:43:02 -0400

    add screen for view chat list

--- lib/presentation/screens/basic-prompt/basic_prompt_screen.dart
+++ lib/presentation/screens/basic-prompt/basic_prompt_screen.dart
@@ -1,4 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as fct;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:uuid/uuid.dart';
+
+final user = fct.User(
+  id: '1',
+  firstName: 'John',
+  lastName: 'Doe',
+  imageUrl: 'https://i.pravatar.cc/150?img=1',
+);
+
+final geminiUser = fct.User(
+  id: '2',
+  firstName: 'Gemini',
+  lastName: 'AI',
+  imageUrl: 'https://picsum.photos/id/179/200/200',
+);
+
+final messages = <fct.Message>[
+  fct.TextMessage(
+    id: Uuid().v4(),
+    text: 'I am fine, thank you!',
+    createdAt: DateTime.now().millisecondsSinceEpoch,
+    author: geminiUser,
+  ),
+  fct.TextMessage(
+    id: Uuid().v4(),
+    text: 'Hello, how are you?',
+    createdAt: DateTime.now().millisecondsSinceEpoch,
+    author: user,
+  ),
+  fct.TextMessage(
+    id: Uuid().v4(),
+    text: 'Hello, world!!',
+    createdAt: DateTime.now().millisecondsSinceEpoch,
+    author: user,
+  ),
+];
 
 class BasicPromptScreen extends StatelessWidget {
   const BasicPromptScreen({super.key});
@@ -7,7 +45,21 @@ class BasicPromptScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('BasicPromptScreen')),
-      body: const Center(child: Text('BasicPromptScreen')),
+      body: Chat(
+        messages: messages,
+        onSendPressed: onSendPressed,
+        user: user,
+        showUserNames: true,
+        showUserAvatars: true,
+        theme: DarkChatTheme(),
+        typingIndicatorOptions: TypingIndicatorOptions(
+          // typingUsers: [geminiUser],
+        ),
+      ),
     );
+  }
+
+  void onSendPressed(fct.PartialText partialText) {
+    print(partialText);
   }
 }
